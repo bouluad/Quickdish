a9a1eafe38c3d60fe74d3cbdf7d02b02af465cfb
bouluad <med.bouluad@gmail.com>
2017-02-27 23:10:25 +0100

    Foundation

---
+++ app/src/androidTest/java/mmm/istic/fr/quickdish/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mmm.istic.fr.quickdish;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/mmm/istic/fr/quickdish/MainActivity.java
@@ -0,0 +1,13 @@
+package mmm.istic.fr.quickdish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
