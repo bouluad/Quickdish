4d495561d407b047361163c45ec4c3e732d8f0ff
bouluad <med.bouluad@gmail.com>
2017-02-27 23:17:27 +0100

    Project structure

--- app/src/main/java/mmm/istic/fr/quickdish/activities/MainActivity.java
+++ app/src/main/java/mmm/istic/fr/quickdish/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package mmm.istic.fr.quickdish;
+package mmm.istic.fr.quickdish.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import mmm.istic.fr.quickdish.R;
 
 public class MainActivity extends AppCompatActivity {
 
