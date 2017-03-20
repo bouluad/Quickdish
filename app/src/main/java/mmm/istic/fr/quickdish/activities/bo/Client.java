a0f5525cd861f5809e721703a40c20191d363cce
bouluad <med.bouluad@gmail.com>
2017-03-20 19:06:13 +0100

    Client object

--- app/src/main/java/mmm/istic/fr/quickdish/activities/bo/Client.java
+++ app/src/main/java/mmm/istic/fr/quickdish/activities/bo/Client.java
@@ -5,4 +5,61 @@ package mmm.istic.fr.quickdish.activities.bo;
  */
 public class Client {
 
+    private int id;
+    private String fullName;
+    private String email;
+    private String address;
+    private int numberOfPoints;
+
+
+    public Client() {
+
+    }
+
+    public Client(int numberOfPoints, String fullName, String email, String address) {
+        this.numberOfPoints = numberOfPoints;
+        this.fullName = fullName;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getNumberOfPoints() {
+        return numberOfPoints;
+    }
+
+    public void setNumberOfPoints(int numberOfPoints) {
+        this.numberOfPoints = numberOfPoints;
+    }
 }
