bdc20def87bd5dc863a3f27f1e953074433b18fe
bouluad <med.bouluad@gmail.com>
2017-03-25 02:48:08 +0100

    Change style buttons

--- app/src/main/java/mmm/istic/fr/quickdish/activities/MainActivity.java
+++ app/src/main/java/mmm/istic/fr/quickdish/activities/MainActivity.java
@@ -3,6 +3,8 @@ package mmm.istic.fr.quickdish.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import mmm.istic.fr.quickdish.R;
@@ -24,5 +26,27 @@ public class MainActivity extends AppCompatActivity {
     public void createCardActivity(View view) {
         Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(myIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
